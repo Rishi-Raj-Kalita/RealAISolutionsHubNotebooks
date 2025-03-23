3ee97e02787e64092a6c6a9c6f0e3fade0c13c55
Rishiraj Kalita <rishirajkalita@Rishirajs-MacBook-Pro.local>
2025-03-23 15:39:37 +0530

    linkedin-summarizer

--- text-to-speech/tts.py
+++ text-to-speech/tts.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from elevenlabs.client import ElevenLabs
-from elevenlabs import play
+from elevenlabs import play, save
 import optparse
 import os
 
@@ -9,10 +9,11 @@ load_dotenv()
 client = ElevenLabs(api_key=os.getenv("ELEVENLABS"), )
 
 audio = client.text_to_speech.convert(
-    text="pallavi is my babeji",
+    text="This is a test audio",
     voice_id="JBFqnCBsd6RMkjVDRZzb",
     model_id="eleven_multilingual_v2",
     output_format="mp3_44100_128",
 )
 
-play(audio)
+# play(audio)
+save(audio, './text-to-speech/audio.wav')
